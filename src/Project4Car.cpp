8e3c669cde1b5e3873102a65a98e9505344afb22
Peyton Elebash <peytayelebash@gmail.com>
2024-10-17 16:08:58 -0500

    filled out most of car setup/planning (not done yet)

--- src/Project4Car.cpp
+++ src/Project4Car.cpp
@@ -52,6 +52,7 @@ void carODE(const ompl::control::ODESolver::StateType & q, const ompl::control::
     */
 
     //TODO: set the velocity and acceleration bounds (not sure if happends here)
+    //TODO: double-check if math correct 
 
     //first grab the control inputs, w (angular velocity) and v (acceleration of vehicle):
     const double *controls = u->as<ompl::control::RealVectorControlSpace::ControlType>()->values;
@@ -75,9 +76,9 @@ void carODE(const ompl::control::ODESolver::StateType & q, const ompl::control::
 
 void makeStreet(std::vector<Rectangle> & obstacles)
 {
-    // TODO: Fill in the vector of rectangles with your street environment.
     //found dimensions for this environment from the project spec
-     Rectangle r1, r2, r3, r4;
+
+    Rectangle r1, r2, r3, r4;
 
     r1.x = -5;
     r1.y = -10;
@@ -106,16 +107,74 @@ void makeStreet(std::vector<Rectangle> & obstacles)
 
 }
 
-ompl::control::SimpleSetupPtr createCar(std::vector<Rectangle> & /* obstacles */)
+ompl::control::SimpleSetupPtr createCar(std::vector<Rectangle> & obstacles)
 {
+    /*
+        Followed along with ompl demo as much as I could: https://ompl.kavrakilab.org/RigidBodyPlanningWithODESolverAndControls_8cpp_source.html
+    */
+
     // TODO: Create and setup the car's state space, control space, validity checker, everything you need for planning.
+
+    //create car's state space: (SE2)
+    auto space(std::make_shared<base::SE2StateSpace());
+
+    //TODO: investigate if these are correct for the environment given in the assignment (x low and high are smaller)
+    base::RealVectorBounds bounds(2);
+    bounds.setLow(-10);
+    bounds.setHigh(10);
+    
+    space->setBounds(bounds);
+
+    //TODO: figure out how to set the control space
+    //TODO: figure out how to set the validity checker correctly
+    //TODO: propogate with the ODE function?
+    //TODO: set start and goal states including the radius for the goal region
+    //TODO: return the simple setup pointer
+
     return nullptr;
 }
 
-void planCar(ompl::control::SimpleSetupPtr &/* ss */, int /* choice */)
+void planCar(ompl::control::SimpleSetupPtr & ss, int choice)
 {
-    // TODO: Do some motion planning for the car
-    // choice is what planner to use.
+    /*
+        choice is what planner to choose:
+        if 1 --> RRT
+        if 2 --> KPIECE
+        if 3 --> RG-RRT
+
+        given a simple setup pointer (ss) and a planner (choice), set up a motion planning problem
+    */
+
+    //set the planner based on choice:
+    //TODO: need to verify if this is the correct method of doing so:
+    if (choice == 1)
+    {
+        ss.setPlanner(new ompl::geometric::RRT(ss.getSpaceInformation()));
+    }
+    if (choice == 2)
+    {
+        ss.setPlanner(new ompl::geometric::KPIECE(ss.getSpaceInformation()));
+    }
+    if (choice == 3)
+    {
+        //TODO: make sure RG-RRT in 'right' format
+        ss.setPlanner(new ompl::geometric::RG-RRT(ss.getSpaceInformation()));
+    }
+
+    //solve the problem:
+    base::PlannerStatus solved = ss.solve(10.0);
+
+    if (solved)
+    {
+        std::cout << "Found Solution:" << std::endl;
+        //convert to geometric path so we can nicely print path as matrix
+        ss.getSolutionPath().asGeometric().printAsMatrix(std::cout);
+    }
+    else
+    {
+        std::cout << "No Slution Found" << std:endl;
+    }
+
 }
 
 void benchmarkCar(ompl::control::SimpleSetupPtr &/* ss */)
